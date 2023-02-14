dc33112d0857a21d0d9820b2c45837dc5de9fa38
krypton225 <k.mohamed.11.98@gmail.com>
2023-02-14 21:40:26 +0200

    ADD Loader module

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import Toggler from "./Toggler.js";
 import Scroll from "./Scroll.js";
 import MainSwiper from "./MainSwiper.js";
+import Loader from "./Loader.js";
+
+Loader.run();
 
 Toggler.click("navbar-toggler", "#navbar-toggler span");
 

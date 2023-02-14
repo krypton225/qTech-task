50be2a2f11361dfa68023141c09b53402978004a
krypton225 <k.mohamed.11.98@gmail.com>
2023-02-14 16:43:29 +0200

    ADD swiper package & ADD first section testimonials

---
+++ js/MainSwiper.js
@@ -0,0 +1,43 @@
+import "../node_modules/swiper/swiper-bundle.js";
+
+const MainSwiper = (function () {
+    function initSwiper1() {
+        new Swiper(".slide-container", {
+            slidesPerView: 4,
+            spaceBetween: 60,
+            sliderPerGroup: 4,
+            loop: true,
+            centerSlide: "true",
+            fade: "true",
+            grabCursor: "true",
+            watchSlidesVisibility: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                767: {
+                    slidesPerView: 2,
+                },
+                1000: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+    }
+
+    return {
+        firstSwiper: initSwiper1,
+    };
+})();
+
+export default MainSwiper;
--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 import Toggler from "./Toggler.js";
 import Scroll from "./Scroll.js";
+import MainSwiper from "./MainSwiper.js";
 
 Toggler.click("navbar-toggler", "#navbar-toggler span");
 
 Scroll.init("navbar");
 
 Scroll.goTop("go-to-top");
+
+MainSwiper.firstSwiper();
